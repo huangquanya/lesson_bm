891128fce8635d4e0b6907d106c457fba4377e57
huangquanya <62825060+huangquanya@users.noreply.github.com>
2020-05-27 21:20:32 +0800

    react-redux初步学习

---
+++ react/react-comment-redux/containers/CommentApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class CommentApp extends Component {
+  render() {
+    return (
+      <div>
+        {/* comment */}
+        
+      </div>
+    )
+  }
+}
---
+++ react/react-comment-redux/reducers/comments.js
@@ -0,0 +1,14 @@
+let comments = [
+    {
+        "id": 1,
+        "username": "liuda",
+        "content": "shuai"
+    }
+]
+
+export default function(state = {
+    comments: comments
+}) {
+    // 初始状态
+    return state
+}
