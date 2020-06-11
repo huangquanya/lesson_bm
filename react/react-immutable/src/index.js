b38c04141cab81f67ab10d43a04d4039b4c376f5
huangquanya <62825060+huangquanya@users.noreply.github.com>
2020-06-11 21:06:59 +0800

    函数式编程，不可变

---
+++ react/react-immutable/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// 可变
+let obj = {}
+let obj1 = obj;
+let obj2 = obj1
+
+obj1.name = 123;
+console.log(obj);
+// 可变是万恶之源
+
+// 不可变
+let newobj = {}
+let obj3 = {
+  ...newobj
+}
+obj3.name = 123;
+console.log(obj3, newobj)
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
