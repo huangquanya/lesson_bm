cfd3709b3c93d68391380ea4b495ccf64c12a564
huangquanya <62825060+huangquanya@users.noreply.github.com>
2020-06-13 16:19:55 +0800

    高阶函数函数式编程 memorize

---
+++ high-order-function/add.js
@@ -0,0 +1,10 @@
+function buildUrl(url, obj) {
+    // 拼接逻辑
+    // 内部的逻辑共有
+
+}
+// 数据不一样
+buildUrl('http://localhost', {search: '海阔天空'})
+buildUrl('http://api.com/v1', {search: '海阔天空'})
+// 1. 不同
+// 2. 共用
